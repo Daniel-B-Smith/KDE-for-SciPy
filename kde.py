e5679a2a42f5398fc8ae5d164f1341495d691173
Daniel B. Smith <Daniel.Smith.Br@gmail.com>
2013-01-23 15:11:05 -0500

    Conversion to SciPy and bug fix.

--- kde.py
+++ kde.py
@@ -6,23 +6,23 @@ estimation via diffusion. The Annals of Statistics, 38(5):2916-2957, 2010.
 
 Based on the implementation in Matlab by Zdravko Botev.
 
-Daniel Smith, PhD
+Daniel B. Smith, PhD
 Updated 1-23-2013
 """
 
 from __future__ import division
 
-import numpy as np
+import scipy as sci
 import scipy.optimize
 import scipy.fftpack
 
-def kde(data, N=2**14, MIN=None, MAX=None):
+def kde(data, N=None, MIN=None, MAX=None):
 
     # Parameters to set up the mesh on which to calculate
-    N = 2**np.ceil(np.log2(N))
+    N = 2**14 if N is None else int(2**sci.ceil(sci.log2(N)))
     if MIN is None or MAX is None:
-        minimum = np.min(data)
-        maximum = np.max(data)
+        minimum = min(data)
+        maximum = max(data)
         Range = maximum - minimum
         MIN = minimum - Range/10 if MIN is None else MIN
         MAX = maximum + Range/10 if MAX is None else MAX
@@ -32,34 +32,43 @@ def kde(data, N=2**14, MIN=None, MAX=None):
 
     # Histogram the data to get a crude first approximation of the density
     M = len(data)
-    DataHist, bins = np.histogram(data,bins=N,range=(MIN,MAX))
+    DataHist, bins = sci.histogram(data, bins=N, range=(MIN,MAX))
     DataHist = DataHist/M
-    DCTData = scipy.fftpack.dct(DataHist, norm='ortho')
+    DCTData = scipy.fftpack.dct(DataHist, norm=None)
 
-    I = (np.arange(N-1)+1)**2
+    I = [iN*iN for iN in xrange(1, N)]
     SqDCTData = (DCTData[1:]/2)**2
 
     # The fixed point calculation finds the bandwidth = t_star
-    t_star = scipy.optimize.brentq(fixed_point,0,0.1,args=(M,I,SqDCTData))
+    guess = 0.1
+    print 'f(0) = '+str(fixed_point(0, M, I, SqDCTData))
+    print 'f(0.1) = '+str(fixed_point(0.1, M, I, SqDCTData))
+
+    try:
+        t_star = scipy.optimize.brentq(fixed_point, 0, guess, 
+                                       args=(M, I, SqDCTData))
+    except ValueError:
+        print 'Oops!'
+        return None
 
     # Smooth the DCTransformed data using t_star
-    SmDCTData = DCTData*np.exp(-np.arange(N)**2*np.pi**2*t_star/2)
+    SmDCTData = DCTData*sci.exp(-sci.arange(N)**2*sci.pi**2*t_star/2)
     # Inverse DCT to get density
-    density = scipy.fftpack.idct(SmDCTData, norm='ortho')*N/R
+    density = scipy.fftpack.idct(SmDCTData, norm=None)*N/R
     mesh = [(bins[i]+bins[i+1])/2 for i in xrange(N)]
-    bandwidth = np.sqrt(t_star)*R
+    bandwidth = sci.sqrt(t_star)*R
     
     return bandwidth, mesh, density
 
 def fixed_point(t, M, I, a2):
     l=7
-    I = np.float128(I)
-    M = np.float128(M)
-    a2 = np.float128(a2)
-    f = 2*np.pi**(2*l)*np.sum(I**7*a2*np.exp(-I*np.pi**2*t))
+    I = sci.float128(I)
+    M = sci.float128(M)
+    a2 = sci.float128(a2)
+    f = 2*sci.pi**(2*l)*sci.sum(I**l*a2*sci.exp(-I*sci.pi**2*t))
     for s in range(l, 1, -1):
-        K0 = np.prod(xrange(1, 2*s, 2))/np.sqrt(2*np.pi)
+        K0 = sci.prod(xrange(1, 2*s, 2))/sci.sqrt(2*sci.pi)
         const = (1 + (1/2)**(s + 1/2))/3
         time=(2*const*K0/M/f)**(2/(3+2*s))
-        f=2*np.pi**(2*s)*np.sum(I**s*a2*np.exp(-I*np.pi**2*time))
-    return t-(2*M*np.sqrt(np.pi)*f)**(-2/5)
+        f=2*sci.pi**(2*s)*sci.sum(I**s*a2*sci.exp(-I*sci.pi**2*time))
+    return t-(2*M*sci.sqrt(sci.pi)*f)**(-2/5)
